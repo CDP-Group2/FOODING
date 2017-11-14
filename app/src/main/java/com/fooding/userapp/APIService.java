f3536de2fa93cf089b5cd5c24285e7fff5258e8d
sooyun802 <sooyun802@gmail.com>
2017-11-14 17:03:09 +0900

    added "get ingredient"

---
+++ app/src/main/java/com/fooding/userapp/APIService.java
@@ -0,0 +1,32 @@
+package com.fooding.userapp;
+
+import com.fooding.userapp.data.model.Ingredient;
+
+import org.json.JSONArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface APIService {
+    public static final String API_URL = "http://poerty.co.kr/fooding/";
+
+    // 음식 key 가지고 해당 음식 재료 정보 가져오기
+    @GET("getIngredient.php")
+    Call<List<Ingredient>>getIngredient(@Query("key") String key);
+
+    // 음식 key로 해당 음식 재료의 이름 가져오기
+    @GET("getIngredientInfo.php")
+    Call<Ingredient>getIngredientInfo(@Query("key") String key);
+
+    // 사업자번호로 해당 사업자 레시 리스트 가져오기
+    @GET("getRecipe.php")
+    Call<ResponseBody>getRecipe(@Query("companyID") String companyID);
+}
